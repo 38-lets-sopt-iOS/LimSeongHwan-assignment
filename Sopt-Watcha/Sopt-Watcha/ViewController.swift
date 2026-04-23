650b8cfd6b3e3c3176b7104a8b235e5df2bc57be
SeongHwan <epaktldk0203@gmail.com>
2026-04-23 20:39:37 +0900

    build: 프로젝트 생성

---
+++ Sopt-Watcha/Sopt-Watcha/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sopt-Watcha
+//
+//  Created by 성환 on 4/23/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
