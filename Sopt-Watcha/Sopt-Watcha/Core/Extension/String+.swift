4bbe8ab41f43f84fed852cea615c2b32301c5261
SeongHwan <epaktldk0203@gmail.com>
2026-04-30 14:24:38 +0900

    fix: 정규식 조건 수정

--- Sopt-Watcha/Sopt-Watcha/Core/Extension/String+.swift
+++ Sopt-Watcha/Sopt-Watcha/Core/Extension/String+.swift
@@ -14,7 +14,7 @@ extension String {
     }
 
     var isValidPassword: Bool {
-        let regex = /^(?=.*[!_@$%^&+=])[A-Za-z0-9!_@$%^&+=]{8,15}$/
+        let regex = /^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[!_@$%^&+=])[A-Za-z0-9!_@$%^&+=]{10,}$/
         return self.wholeMatch(of: regex) != nil
     }
 }
