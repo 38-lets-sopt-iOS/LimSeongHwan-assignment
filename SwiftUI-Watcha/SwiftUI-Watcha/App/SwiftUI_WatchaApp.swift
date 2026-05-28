294116fa0f949e80eb8c18ba17ecb0134d6d00ad
SeongHwan <epaktldk0203@gmail.com>
2026-05-29 02:30:25 +0900

    build: 프로젝트 생성

---
+++ SwiftUI-Watcha/SwiftUI-Watcha/App/SwiftUI_WatchaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WatchaApp.swift
+//  SwiftUI-Watcha
+//
+//  Created by 성환 on 5/28/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WatchaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
